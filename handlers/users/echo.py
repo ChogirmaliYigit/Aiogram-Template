e9f551824b38115cff3616a33ead23ffd9f40702
Jahongir <chogirmali.yigit@gmail.com>
2023-03-17 13:26:30 +0500

    Yangilandi

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -1,11 +1,8 @@
 from aiogram import types
 from loader import dp
-from keyboards.default.main import cats_markup
-from states.shop import ShopState
 
-
-@dp.message_handler(text="🛍 Buyurtma berish")
+# Echo bot
+@dp.message_handler(state=None)
 async def bot_echo(message: types.Message):
-    await message.answer(text="Nimadan boshlaymiz?", reply_markup=cats_markup)
-    await ShopState.category.set()
+    await message.answer(text=message.text)
 
