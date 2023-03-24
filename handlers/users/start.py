7113430fc608d13d4e3e5a76097fca95b21ebeb9
Jahongir Ibragimov <118691897+ChogirmaliYigit@users.noreply.github.com>
2023-03-24 14:13:57 +0500

    start.py yangilandi

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -12,8 +12,7 @@ async def bot_start(message: types.Message):
     if not name.startswith('<') and not name.endswith('>'):
         # Foydalanuvchini bazaga qo'shamiz
         try:
-            db.add_user(id=message.from_user.id,
-                        name=name)
+            db.add_user(id=message.from_user.id, name=name)
             await message.answer(f"Xush kelibsiz! {name}")
             # Adminga xabar beramiz
             count = db.count_users()[0]
@@ -24,10 +23,35 @@ async def bot_start(message: types.Message):
             await bot.send_message(chat_id=ADMINS[0], text=f"{name} bazaga oldin qo'shilgan")
             await message.answer(f"Xush kelibsiz! {name}")
 
+        except Exception as err:
+            await bot.send_message(chat_id=ADMINS[0], text=f"{name} bazaga oldin qo'shilgan")
+            await message.answer(f"Xush kelibsiz! {name}")
+
     else:
         if message.from_user.username:
-            await bot.send_message(chat_id=ADMINS[0], text=f"@{message.from_user.username} bazaga oldin qo'shilgan yoki qo'shildi!")
-            await message.answer(f"Xush kelibsiz! @{message.from_user.username}")
+            try:
+                db.add_user(id=message.from_user.id, name=name)
+                msg = f"@{message.from_user.username} bazaga qo'shildi.\nBazada {count} ta foydalanuvchi bor."
+                await bot.send_message(chat_id=ADMINS[0], text=msg)
+
+            except sqlite3.IntegrityError as err:
+                await bot.send_message(chat_id=ADMINS[0], text=f"@{message.from_user.username} bazaga oldin qo'shilgan")
+                await message.answer(f"Xush kelibsiz! @{message.from_user.username}")
+            
+            except Exception as err:
+                await bot.send_message(chat_id=ADMINS[0], text=f"@{message.from_user.username} bazaga oldin qo'shilgan")
+                await message.answer(f"Xush kelibsiz! @{message.from_user.username}")
+
         else:
-            await bot.send_message(chat_id=ADMINS[0], text=f"{message.from_user.id} bazaga oldin qo'shilgan yoki qo'shildi!")
-            await message.answer(f"Xush kelibsiz!")
+            try:
+                db.add_user(id=message.from_user.id, name=name)
+                msg = f"{message.from_user.id} bazaga qo'shildi.\nBazada {count} ta foydalanuvchi bor."
+                await bot.send_message(chat_id=ADMINS[0], text=msg)
+
+            except sqlite3.IntegrityError as err:
+                await bot.send_message(chat_id=ADMINS[0], text=f"{message.from_user.id} bazaga oldin qo'shilgan")
+                await message.answer(f"Xush kelibsiz!")
+            
+            except Exception as err:
+                await bot.send_message(chat_id=ADMINS[0], text=f"{message.from_user.id} bazaga oldin qo'shilgan")
+                await message.answer(f"Xush kelibsiz!")
